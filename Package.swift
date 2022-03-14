be043849dc2322f89bd259348e84974267940fd9
Oleksii Hurin <dn070287gav@gmail.com>
2022-03-14 07:53:54 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ElkycDocumentTools",
+    products: [
+        .library(
+            name: "ElkycDocumentTools",
+            targets: ["ElkycDocumentTools"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ElkycDocumentTools",
+            url: "https://pods.regulaforensics.com/RegulaCommon/6.2.110/RegulaCommon-6.2.110.zip",
+            checksum: "7fa3526a3a5bcff0ecdd6947b9b05b88c0163b0c2df47c9bb4903d25307fa4fc")
+    ]
+)
